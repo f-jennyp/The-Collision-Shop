1606369f4220351fb8fac4f241d28a0973d00444
Jenny <fernandez.jennyp@gmail.com>
2023-12-29 12:02:39 +0800

    commit

---
+++ app/view/home.php
@@ -0,0 +1,189 @@
+<div id="content">
+
+	<div id="services">
+		<div class="row">
+			<h1>The Collision Shop Detroit’s Services</h1>
+
+			<div class="services-con">
+				<div class="box">
+					<div class="title">Auto Body & Paint Repaint</div>
+				</div>
+			</div>
+
+			<div class="services-con">
+				<div class="box">
+					<div class="title">Collision Repair</div>
+					<p>We will put your vehicle back to pre-accident condition, structurally and cosmetically.</p>
+				</div>
+			</div>
+
+			<div class="services-con">
+				<div class="box">
+					<div class="title">Auto Glass Repair</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div id="about">
+		<div class="row">
+			<div class="right">
+				<div class="img-holder">
+					<img src="public/images/content/" alt="">
+				</div>
+			</div>
+
+			<div class="left">
+				<h1>About The Collision Shop Detroit</h1>
+				<p>The Collision Shop has been in business for over 28 years. We have grown to the largest Auto Body &
+					Paint repair facility serving the Greater Detroit area in Michigan. With multiple locations
+					conveniently located throughout the metro Detroit area, any where that you work or live you are less
+					than twenty minutes away from any one of our locations.
+
+					We try to accommodate our customers the best way that we know how and we will provide pick up and
+					drop off at your home or work if necessary. The Collision Shop Detroit is equipped with
+					state-of-the-art auto body equipment necessary to repair the new complex vehicles of today.
+
+					Our customers have come to know and trust The Collision Shop Detroit of Detroit, MI for saving their
+					insurance deductible or providing a free loaner vehicle.
+
+					A program that we fist started in order to help our customers save time and money with their
+					repairs. Coupled with our Lifetime Warranty, why would you have your vehicle repaired any where
+					else?</p>
+
+				<div class="btn-holder">
+					<div class="btn">
+						<a href="<?php echo URL ?>contact#content">Contact The Collision Shop Detroit</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="section">
+		<div class="row">
+			<h2>We Are The Auto Body Repair Professionals</h2>
+			<p>Contact us today and let us show you how easy it is to have your vehicle repaired at The Collision Shop
+				Detroit of Detroit, MI.</p>
+			<div class="btn-holder">
+				<div class="btn">
+					<a href="<?php echo URL ?>contact#content">Contact The Collision Shop Detroit</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="gallery">
+		<div class="row flex">
+			<h1>The Collision Shop Detroit Gallery</h1>
+			<div id="gall1" class="gallery-container">
+				<ul class="gallery">
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/1.png">
+							<img class="img-responsive" src="public/images/gallery/1.jpg" alt="gallery">
+						</a>
+					</li>
+
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/2.png">
+							<img class="img-responsive" src="public/images/gallery/2.jpg" alt="gallery">
+						</a>
+					</li>
+
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/3.png">
+							<img class="img-responsive" src="public/images/gallery/3.jpg" alt="gallery">
+						</a>
+					</li>
+
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/4.png">
+							<img class="img-responsive" src="public/images/gallery/4.jpg" alt="gallery">
+						</a>
+					</li>
+
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/5.png">
+							<img class="img-responsive" src="public/images/gallery/5.jpg" alt="gallery">
+						</a>
+					</li>
+
+					<li>
+						<a data-fancybox-group="default" class="thumbnail fancy" title="gallery"
+							href="public/images/gallery/6.png">
+							<img class="img-responsive" src="public/images/gallery/6.jpg" alt="gallery">
+						</a>
+					</li>
+
+
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div id="testimomials">
+		<div class="row">
+			<h2>Customer Reviews</h2>
+
+			<div class="box">
+				<div class="remarks">“ The repair and replacement of my fender, bumper and hood from damage due to a car
+					crash was amazing. My car is looking good again. Excellent work. They definitely takes their time to
+					get it right. I’m a satisfied customer. ”</div>
+
+				<div class="auth">Trae L.</div>
+				<div class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="contact">
+		<div class="row">
+			<div class="foreword">KEEP IN TOUCH</div>
+			<h1>Contact Form</h1>
+			<div class="to-sm">We will be glad to answer your questions, feel free to ask a piece of information or a
+				quotation. <span class="to-db"> We are looking forward to work with you.</span></div>
+			<form action="sendContactForm" method="post" class="sends-email ctc-form">
+				<div class="flex">
+					<label><span class="ctc-hide">Name</span>
+						<input type="text" name="name" placeholder="Name">
+					</label>
+					<label><span class="ctc-hide">Email</span>
+						<input type="text" name="email" placeholder="Email">
+					</label>
+					<label><span class="ctc-hide">Phone</span>
+						<input type="text" name="phone" placeholder="Phone">
+					</label>
+				</div>
+				<label><span class="ctc-hide">Message</span>
+					<textarea name="message" cols="30" rows="10" placeholder="Message"></textarea>
+				</label>
+				<label for="g-recaptcha-response"><span class="ctc-hide">Recaptcha</span></label>
+				<div class="g-recaptcha"></div>
+
+				<div class="flex">
+					<label>
+						<input type="checkbox" name="consent" class="consentBox">I hereby consent to having this website
+						store my submitted information so that they can respond to my inquiry.
+					</label><br>
+					<?php if ($this->siteInfo['policy_link']): ?>
+						<label>
+							<input type="checkbox" name="termsConditions" class="termsBox" /> I hereby confirm that I have
+							read
+							and understood this website’s <a href="<?php $this->info("policy_link"); ?>"
+								target="_blank">Privacy
+								Policy.</a>
+						</label>
+					<?php endif ?>
+					<button type="submit" class="ctcBtn btn" disabled>Submit Form</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+</div>
---
+++ app/view/template/header.php
@@ -0,0 +1,121 @@
+<?php $this->suspensionRedirect($view); ?>
+<!DOCTYPE html>
+<html lang="en" <?php $this->helpers->htmlClasses(); ?>>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+	<?php $this->helpers->seo($view); ?>
+	<link rel="icon" href="public/images/favicon.png" type="image/x-icon">
+	<!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" />
+	<?php $this->helpers->analytics(); ?>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body <?php $this->helpers->bodyClasses($view); ?>>
+	<?php $this->checkSuspensionHeader(); ?>
+	<header>
+		<div id="header">
+			<div class="header-con row flex">
+				<div class="left logo-holder">
+					<a href="home"><img src="public/images/content/logo.png" alt="Logo"></a>
+				</div>
+
+				<div class="right">
+					<div class="nav-holder">
+						<nav>
+							<a href="#" id="pull"><strong>MENU</strong></a>
+							<ul>
+								<li <?php $this->helpers->isActiveMenu("home"); ?>><a href="<?php echo URL ?>">HOME</a>
+								</li>
+								<li <?php $this->helpers->isActiveMenu("about"); ?>><a
+										href="<?php echo URL ?>about#content">ABOUT US</a></li>
+								<li <?php $this->helpers->isActiveMenu("services"); ?>><a
+										href="<?php echo URL ?>services#content">SERVICES</a></li>
+								<li <?php $this->helpers->isActiveMenu("testimonials"); ?>><a
+										href="<?php echo URL ?>testimonials#content">TESTIMONIALS</a></li>
+								<li <?php $this->helpers->isActiveMenu("gallery"); ?>><a
+										href="<?php echo URL ?>gallery#content">GALLERY</a></li>
+								<li <?php $this->helpers->isActiveMenu("contact"); ?>><a
+										href="<?php echo URL ?>contact#content">CONTACT US</a></li>
+							</ul>
+						</nav>
+					</div>
+
+					<div class="comp-info flex">
+						<div class="address flex">
+							<img src="public/images/content/ban-add.png" alt="address">
+							<div class="text-left">
+								<a>
+									<?php $this->info("address"); ?>
+								</a>
+							</div>
+						</div>
+
+						<div class="email flex">
+							CALL US!
+							<div class="text-left">
+								<a href="mailto:<?php $this->info("email"); ?>">
+									<?php $this->info("email"); ?>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- <?php //if($view == "home"):?> -->
+	<div id="banner">
+		<div class="row banner-con">
+			<div class="top">
+				<div class="ban-title">We Pay Your Deductible* <br> Free Loaner Vehicle*</div>
+				<span class="to-sm">*(some restrictions apply - please contact us for details)</span>
+
+				<div class="btn-holder">
+					<div class="btn">
+						<a href="<?php echo URL ?>contact#content">Contact The Collision Shop Detroit</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="ban-bottom">
+		<div class="bottom-con">
+			<div class="content flex">
+				<div class="box">
+					<div class="img-holder img1">
+						<img src="public/images/content/ban-img1.png" alt="">
+					</div>
+					<div>Serving The Detroit, MI Area</div>
+				</div>
+
+				<div class="box">
+					<div class="img-holder img2">
+						<img src="public/images/content/ban-img2.png" alt="">
+					</div>
+					<div>Insurance Repair Specialist</div>
+				</div>
+
+				<div class="box">
+					<div class="img-holder img3">
+						<img src="public/images/content/ban-img3.png" alt="">
+					</div>
+					<div>Lifetime Warranty</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- <?php //endif; ?> -->
